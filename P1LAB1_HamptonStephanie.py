141568da8d927a3ce3463d6b1a472051fa2c2823
hamptons0463 <hamptons0463@student.faytechcc.edu>
2025-02-06 18:49:16 -0500

    Add files via upload
    P1LAB1

---
+++ P1LAB1_HamptonStephanie.py
@@ -0,0 +1,32 @@
+#Stephanie Hampton
+#02/06/2025
+#P1LAB1
+###first name last name greeting
+
+'''nananana'''
+
+##request for first name
+
+print ("Enter your first name:")
+
+##assigning fname to user input of first name
+
+fname = input()
+
+##request for last name
+
+print ("Enter your last name:")
+
+##assigning lname to user input of last name
+
+lname = input()
+
+#spacing
+
+print()
+print()
+
+#personalized greeting
+
+print("Hello,", fname, lname,"and welcome to CTI-110!")
+print("I hope you enjoy your time here!")
